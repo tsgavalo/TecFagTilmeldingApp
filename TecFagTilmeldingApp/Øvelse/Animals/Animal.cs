059983b12f8ea02b8007ef289cf814dc26448db1
Niels Olesen <no@tec.dk>
2023-08-26 12:05:48 +0200

    init

---
+++ TecFagTilmeldingApp/Øvelse/Animals/Animal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TecFagTilmeldingApp.Øvelse.Animals;
+
+internal class Animal
+{
+    public virtual string MakeSound() => "First choose an amimal.";
+}
---
+++ TecFagTilmeldingApp/Øvelse/Animals/Cat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TecFagTilmeldingApp.Øvelse.Animals;
+
+internal class Cat : Animal
+{
+    public override string MakeSound()
+    {
+        string myCurrentDer = System.IO.Directory.GetCurrentDirectory();
+        string appDir = System.IO.Path.Combine(myCurrentDer, "Øvelse");
+        appDir = System.IO.Path.Combine(appDir, "Animals");
+        appDir = System.IO.Path.Combine(appDir, "cat.wav");
+
+        System.Media.SoundPlayer soundPlayer = new System.Media.SoundPlayer();
+        soundPlayer.SoundLocation = appDir;
+        soundPlayer.Play();
+
+        return "You have choosen a cat.";
+    }
+}
